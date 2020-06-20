bb0a2e583d0339fda0f453b248788fa2d98d69fd
YannisCheng <cwj1714@163.com>
2020-06-21 00:13:40 +0800

    单向链表基本操作

--- src/base/data_structure/linked_list/LinkedListTest.java
+++ src/base/data_structure/linked_list/LinkedListTest.java
@@ -4,11 +4,39 @@ public class LinkedListTest {
 
     public static void main(String[] args) {
         SingleLinkedList singleLinkedList = new SingleLinkedList();
-        singleLinkedList.push(new BookNode("ren",0));
-        singleLinkedList.push(new BookNode("ren",1));
-        singleLinkedList.push(new BookNode("ren",2));
-        singleLinkedList.push(new BookNode("ren",3));
-        singleLinkedList.push(new BookNode("ren",4));
+
+        BookNode node0 = new BookNode("ren", 0);
+        BookNode node1 = new BookNode("ren", 1);
+        BookNode node2 = new BookNode("ren", 2);
+        BookNode node3 = new BookNode("ren", 3);
+        BookNode node4 = new BookNode("ren", 4);
+        BookNode node5 = new BookNode("ren", 5);
+        BookNode node6 = new BookNode("ren", 6);
+        BookNode node7 = new BookNode("ren", 7);
+        BookNode node8 = new BookNode("ren", 8);
+        BookNode node9 = new BookNode("ren", 9);
+        BookNode node10 = new BookNode("ren", 10);
+
+        // 尾插测试
+        singleLinkedList.pushOrder(node0);
+        singleLinkedList.pushOrder(node1);
+        singleLinkedList.pushOrder(node2);
+        singleLinkedList.pushOrder(node3);
+        singleLinkedList.pushOrder(node4);
+        singleLinkedList.len();
+        singleLinkedList.forEach();
+        // 头插测试
+        singleLinkedList.pushFront(node5);
+        singleLinkedList.pushFront(node6);
+        singleLinkedList.pushFront(node7);
+        singleLinkedList.pushFront(node8);
+        singleLinkedList.pushFront(node9);
+        singleLinkedList.len();
+        singleLinkedList.forEach();
+        System.out.println("---------------");
+        singleLinkedList.remove(node2);
+        singleLinkedList.remove(node7);
+        singleLinkedList.len();
         singleLinkedList.forEach();
 
     }
--- src/base/data_structure/linked_list/SingleLinkedList.java
+++ src/base/data_structure/linked_list/SingleLinkedList.java
@@ -9,12 +9,34 @@ public class SingleLinkedList {
 
     public SingleLinkedList() {
         head = new BookNode();
+        System.out.println("head is " + head.toString());
     }
 
-    public void push(BookNode node){
+    private void nullCheck(BookNode node) {
         if (node == null) {
             throw new NullPointerException();
         }
+    }
+
+    /**
+     * 头插 - 栈的实现方式
+     *
+     * @param node 待插入节点
+     */
+    public void pushFront(BookNode node) {
+        //1.更新node节点的next
+        node.setNext(head.getNext());
+        //2.更新head
+        head.setNext(node);
+    }
+
+    /**
+     * 尾插：顺序插入链表元素 - 队列的实现方式
+     *
+     * @param node 链表元素
+     */
+    public void pushOrder(BookNode node) {
+        nullCheck(node);
 
         if (head.getNext() == null) {
             head.setNext(node);
@@ -28,13 +50,60 @@ public class SingleLinkedList {
     }
 
     /**
+     * 删除节点
+     */
+    public void remove(BookNode node) {
+        BookNode temp = head;
+        if (temp.getNext() == node) {
+            temp.setNext(null);
+        }
+        while (temp != null) {
+            if (temp.getNext() == node) {
+                System.out.println("删除的元素为："+temp.getNext().getNumIndex());
+                temp.setNext(temp.getNext().getNext());
+                break;
+            }
+            temp = temp.getNext();
+        }
+    }
+
+
+    /**
+     * 更新节点
+     */
+    public void upDataNode() {
+
+    }
+
+    /**
+     * 插入节点
+     */
+    public void insertNode(BookNode node, int index) {
+        BookNode temp = head;
+        while (temp.getNext() != null) {
+
+        }
+    }
+
+    public int len() {
+        BookNode temp = head;
+        int length = 0;
+        while (temp.getNext() != null) {
+            length++;
+            temp = temp.getNext();
+        }
+        System.out.println("长度len为：" + length);
+        return length;
+    }
+
+    /**
      * 遍历
      */
-    public void forEach(){
-        BookNode temp = head.getNext();
+    public void forEach() {
+        BookNode temp = head;
         while (temp.getNext() != null) {
-            System.out.println(temp);
-            System.out.println(temp.getNumIndex());
+            System.out.println(temp.getNext());
+            System.out.println(temp.getNext().getNumIndex());
             temp = temp.getNext();
         }
     }
