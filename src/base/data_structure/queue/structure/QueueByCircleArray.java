38bffe2df3a18464e301699cc07d1502fdb3002a
YannisCheng <cwj1714@163.com>
2020-06-22 17:49:39 +0800

    数组环状队列修改

--- src/base/data_structure/queue/structure/QueueByCircleArray.java
+++ src/base/data_structure/queue/structure/QueueByCircleArray.java
@@ -5,4 +5,45 @@ package base.data_structure.queue.structure;
  * 2020-6-21 21:12:04
  */
 public class QueueByCircleArray {
+
+    private int maxSize;
+    private int font;
+    private int rear;
+    private String[] queue = null;
+
+    public QueueByCircleArray(int maxSize) {
+        this.maxSize = maxSize;
+        this.queue = new String[maxSize];
+    }
+
+    /**
+     * 是否已满
+     *
+     * @return true：满
+     */
+    public boolean isFull() {
+
+    }
+
+    /**
+     * 是否为空
+     *
+     * @return true 空
+     */
+    public boolean isEmpty() {
+
+    }
+
+    public boolean push(String item){
+
+    }
+
+    public String pop(){
+
+    }
+
+    public void forEach(){
+
+    }
+
 }
