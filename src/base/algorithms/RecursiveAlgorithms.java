c896e7fa62d1cb3dd3eb974c6681db1cc8556f4c
YannisCheng <cwj1714@163.com>
2020-06-30 13:36:02 +0800

    问题实现

--- src/base/algorithms/RecursiveAlgorithms.java
+++ src/base/algorithms/RecursiveAlgorithms.java
@@ -43,6 +43,17 @@ public class RecursiveAlgorithms {
 
         // 倒序输出一个正整数
         aboutReverseOutputInteger();
+
+        // 青蛙跳台阶问题
+        frogJumpingSteps();
+    }
+
+    /**
+     * 青蛙跳台阶问题
+     * 问题描述：一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+     */
+    private static void frogJumpingSteps() {
+
     }
 
     /**
