3e8a542eb891b5e69ec665d9b2a17a77ec3e86ff
YannisCheng <cwj1714@163.com>
2020-07-01 00:11:09 +0800

    Threadlocal 了解

--- src/java_vm/thrad_local/ThreadLocalTestUtils.java
+++ src/java_vm/thrad_local/ThreadLocalTestUtils.java
@@ -2,6 +2,8 @@ package java_vm.thrad_local;
 
 /**
  * ThreadLocalTestUtils ThreadLocal测试工具类
+ * 用例参考：https://mp.weixin.qq.com/s/TOWo4RcIda7s_e5h08I3zg
+ * 原理讲解：https://blog.csdn.net/puppylpg/article/details/80433271?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-10.nonecase&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-10.nonecase
  *
  * @author yannischeng  cwj1714@163.com
  * @date 2020/6/28 - 16:53
