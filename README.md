5201778f8cbb4e2cee1e3259b516e1d52f4bfe7f
YannisCheng <cwj1714@163.com>
2020-07-02 21:39:33 +0800

    README.md修改

---
+++ README.md
@@ -0,0 +1,4 @@
+---
+2020-7-2 21:37:43 周四
+
+[Java8 中文在线API文档](https://www.matools.com/api/java8)
--- src/java_vm/thread_pool/ThreadFactoryTestUtils.java
+++ src/java_vm/thread_pool/ThreadFactoryTestUtils.java
@@ -3,8 +3,28 @@ package java_vm.thread_pool;
 /**
  * ThreadFactoryTestUtils 线程池测试工具类
  *
+ * <br><p>
+ * 线程池的7个参数；
+ * <p>
+ * 1）corePoolSize   ：核心线程数
+ * <p>
+ * 2）maximumPoolSize：最大线程数
+ * <p>
+ * 3）keepAliveTime  ：空闲线程存活时间
+ * <p>
+ * 4）unit           ：空闲线程存活时间单位
+ * <p>
+ * 5）workQueue      ：工作队列
+ * <p>
+ * 6）threadFactory  ：线程工厂
+ * <p>
+ * 7）handler        ：拒绝策略
+ *
+ * <br><p>
+ *
  * @author yannischeng  cwj1714@163.com
  * @date 2020/6/28 - 16:52
  */
 public class ThreadFactoryTestUtils {
+
 }
