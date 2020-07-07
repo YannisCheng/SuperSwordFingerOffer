5848a4536978579cbe9a961e2ef8c369443ec811
YannisCheng <cwj1714@163.com>
2020-07-08 00:51:16 +0800

    README.md修改

--- src/base/algorithms/ExercisePad.java
+++ src/base/algorithms/ExercisePad.java
@@ -12,17 +12,23 @@ public class ExercisePad {
     private static final int[] ARR = {3, 5, 1, 9, 6, 8, 4, 7, 2};
 
     public static void main(String[] args) {
-
+        chooseSort(ARR);
 
     }
 
-    /**
-     *      A       B       C
-     * 1层：A->C
-     * 2层：
-     * 3层：
-     *
-     */
+    private static void chooseSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int index = i;
+            while (index>0 && temp < arr[index-1]) {
+                arr[index]= arr[index-1];
+                index--;
+            }
+            arr[index] = temp;
+        }
+        showSort();
+    }
+
     public static void showSingleSort(int i) {
         System.out.println("第" + (i + 1) + "次的结果为：" + Arrays.toString(ARR));
     }
