9a6b6711885e571656d5f4b1daa73926dfbf0682
YannisCheng <cwj1714@163.com>
2020-07-07 09:13:43 +0800

    线程池参数配置：

--- src/java_vm/thread_pool/About_ThreadPool.md
+++ src/java_vm/thread_pool/About_ThreadPool.md
@@ -26,12 +26,16 @@
  workQueue      ：工作队列
                  - 直接提交队列：SynchronousQueue。没有容量，每执行一个插入操作就会阻塞，需要再执行一个删除操作才会被唤醒，反之每一个删除操作也都要等待对应的插入操作。
                  - 有界任务队列：ArrayBlockingQueue。需设置 "初始容量"。
-                 - 无界任务队列：LinkedBlockingQueue。任务队列可以无限制的添加新的任务，最大线程数为corePoolSize值，此时maximumPoolSize的值是无效的
-                 - 优先任务队列：PriorityBlockingQueue。特殊的无界队列
+                 - 无界任务队列：LinkedBlockingQueue。任务队列可以无限制的添加新的任务，最大线程数为corePoolSize值，此时maximumPoolSize的值是无效的，先进先出的规则
+                 - 优先任务队列：PriorityBlockingQueue。特殊的无界队列，自定义规则，根据任务的优先级顺序先后执行
 
  threadFactory  ：线程工厂
 
- handler        ：拒绝策略
+ handler        ：拒绝策略（处理线程池"超载"的情况）
+                - AbortPolicy策略：该策略会直接抛出异常，阻止系统正常工作；
+                - CallerRunsPolicy策略：如果线程池的线程数量达到上限，该策略会把任务队列中的任务放在调用者线程当中运行；
+                - DiscardOledestPolicy策略：该策略会丢弃任务队列中最老的一个任务（也就是当前任务队列中最先被添加进去的，马上要被执行的那个任务）并尝试再次提交；
+                - DiscardPolicy策略：该策略会默默丢弃无法处理的任务，不予任何处理。当然使用此策略，业务场景中需允许任务的丢失；
  
 ## 使用
 
--- src/java_vm/thread_pool/ThreadFactoryTestUtils.java
+++ src/java_vm/thread_pool/ThreadFactoryTestUtils.java
@@ -1,7 +1,7 @@
 package java_vm.thread_pool;
 
 import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -19,12 +19,18 @@ import java.util.concurrent.TimeUnit;
  * <p>
  * 4）unit           ：空闲线程存活时间单位
  * <p>
- * 5）workQueue      ：工作队列
+ * 5）workQueue      ：工作队列-4种
  * <p>
- * 6）threadFactory  ：线程工厂
+ * 6）threadFactory  ：线程工厂-自定义
  * <p>
- * 7）handler        ：拒绝策略
+ * 7）handler        ：拒绝策略-4种
  *
+ * <br><p>
+ * 线程池线程数量设置：
+ * Nthreads=CPU数量
+ * Ucpu=目标CPU的使用率，0<=Ucpu<=1
+ * W/C=任务等待时间与任务计算时间的比率
+ * Nthreads = Ncpu*Ucpu*(1+W/C);
  * <br><p>
  *
  * @author yannischeng  cwj1714@163.com
@@ -32,22 +38,57 @@ import java.util.concurrent.TimeUnit;
  */
 public class ThreadFactoryTestUtils {
     public static void main(String[] args) {
+        // 线程池参数配置：
+        // 1：4种不同任务队列的使用
+        // 2：4种不同拒绝策略的使用
+        // 3：自定义ThreadFactory
+        // 4：ThreadPoolExecutor扩展
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
                 2,
                 4,
                 10,
                 TimeUnit.MILLISECONDS,
                 new ArrayBlockingQueue<Runnable>(4),
-                Executors.defaultThreadFactory(),
-                new ThreadPoolExecutor.AbortPolicy());
+                //Executors.defaultThreadFactory(),
+                new ThreadFactory() {
+                    @Override
+                    public Thread newThread(Runnable r) {
+                        System.out.println("线程: " + r.hashCode() + "被创建");
+                        return new Thread(r, "threadPool: " + r.hashCode());
+                    }
+                },
+                new ThreadPoolExecutor.DiscardOldestPolicy()) {
+            // ThreadPoolExecutor扩展
+            @Override
+            protected void beforeExecute(Thread t, Runnable r) {
+                super.beforeExecute(t, r);
+                System.out.println("线程池中任务运行前执行,准备执行：" + t.getName());
+            }
+
+            @Override
+            protected void afterExecute(Runnable r, Throwable t) {
+                super.afterExecute(r, t);
+                System.out.println("线程池中任务运行完毕后执行,执行完毕：");
+            }
+
+            @Override
+            protected void terminated() {
+                super.terminated();
+                System.out.println("线程池退出后执行,线程池退出");
+            }
+        };
 
         for (int i = 0; i < 10; i++) {
             executor.execute(new ThreadTaskArray(i));
         }
+
+        // 当线程池调用该方法后，线程池中不再接受后续添加的任务。
+        // 但是，此时线程池不会立刻退出，直到添加到线程池中的任务都已经处理完成，才会退出。
+        executor.shutdown();
     }
 
     /**
-     * workQueue类型为：SynchronousQueue，运行结果为：
+     * workQueue类型为：SynchronousQueue，拒绝策略：AbortPolicy，运行结果为：
      *
      * Exception in thread "main" java.util.concurrent.RejectedExecutionException: Task java_vm.thread_pool.ThreadTaskArray@61bbe9ba rejected from java.util.concurrent.ThreadPoolExecutor@610455d6[Running, pool size = 4, active threads = 4, queued tasks = 0, completed tasks = 0]
      * 	at java.util.concurrent.ThreadPoolExecutor$AbortPolicy.rejectedExecution(ThreadPoolExecutor.java:2047)
@@ -58,10 +99,22 @@ public class ThreadFactoryTestUtils {
      * index:0, ThreadName:pool-1-thread-1
      * index:2, ThreadName:pool-1-thread-3
      * index:3, ThreadName:pool-1-thread-4
+     *
+     * ~ 拒绝策略：AbortPolicy，运行结果为:
+     * index:0, ThreadName:pool-1-thread-1
+     * index:2, ThreadName:pool-1-thread-3
+     * index:1, ThreadName:pool-1-thread-2
+     * index:4, ThreadName:main
+     * index:3, ThreadName:pool-1-thread-4
+     * index:7, ThreadName:pool-1-thread-4
+     * index:9, ThreadName:main
+     * index:5, ThreadName:pool-1-thread-3
+     * index:8, ThreadName:pool-1-thread-1
+     * index:6, ThreadName:pool-1-thread-2
      */
 
     /**
-     * workQueue类型为：ArrayBlockingQueue，运行结果为：
+     * workQueue类型为：ArrayBlockingQueue，拒绝策略：AbortPolicy，运行结果为：
      *
      * Exception in thread "main" java.util.concurrent.RejectedExecutionException: Task java_vm.thread_pool.ThreadTaskArray@6e0be858 rejected from java.util.concurrent.ThreadPoolExecutor@61bbe9ba[Running, pool size = 4, active threads = 4, queued tasks = 4, completed tasks = 0]
      * 	at java.util.concurrent.ThreadPoolExecutor$AbortPolicy.rejectedExecution(ThreadPoolExecutor.java:2047)
@@ -76,10 +129,32 @@ public class ThreadFactoryTestUtils {
      * index:5, ThreadName:pool-1-thread-2
      * index:4, ThreadName:pool-1-thread-3
      * index:3, ThreadName:pool-1-thread-4
+     *
+     * ~ 拒绝策略：CallerRunsPolicy，运行结果为:
+     * index:6, ThreadName:pool-1-thread-3
+     * index:8, ThreadName:main
+     * index:7, ThreadName:pool-1-thread-4
+     * index:0, ThreadName:pool-1-thread-1
+     * index:1, ThreadName:pool-1-thread-2
+     * index:2, ThreadName:pool-1-thread-3
+     * index:4, ThreadName:pool-1-thread-1
+     * index:3, ThreadName:pool-1-thread-4
+     * index:5, ThreadName:pool-1-thread-2
+     * index:9, ThreadName:pool-1-thread-3
+     *
+     *  ~ 拒绝策略：DiscardOldestPolicy，运行结果为:
+     * index:7, ThreadName:pool-1-thread-4
+     * index:0, ThreadName:pool-1-thread-1
+     * index:6, ThreadName:pool-1-thread-3
+     * index:1, ThreadName:pool-1-thread-2
+     * index:4, ThreadName:pool-1-thread-4
+     * index:9, ThreadName:pool-1-thread-2
+     * index:8, ThreadName:pool-1-thread-3
+     * index:5, ThreadName:pool-1-thread-1
      */
 
     /**
-     * workQueue类型为：LinkedBlockingQueue，运行结果为：
+     * workQueue类型为：LinkedBlockingQueue，拒绝策略：AbortPolicy，运行结果为：
      *
      * index:0, ThreadName:pool-1-thread-1
      * index:1, ThreadName:pool-1-thread-2
@@ -94,7 +169,7 @@ public class ThreadFactoryTestUtils {
      */
 
     /**
-     * workQueue类型为：PriorityBlockingQueue时，运行结果为：
+     * workQueue类型为：PriorityBlockingQueue时，拒绝策略：AbortPolicy，运行结果为：
      *
      * priority:1,ThreadName:pool-1-thread-2
      * priority:0,ThreadName:pool-1-thread-1
